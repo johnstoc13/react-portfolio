ab855f1004bbbc38bdc8892d21a117b9cf2f23b6
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-11-06 23:07:40 -0500

    portfolio page working!

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "../components/Grid";
+import Portfolios from "../components/Portfolios";
+
+const Portfolio = () => {
+    return (
+        <Container fluid>
+            <Portfolios />
+        </Container>
+    );
+};
+
+export default Portfolio;
