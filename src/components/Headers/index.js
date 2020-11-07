fc7a3da6fb8acc4f3ae20867e36ccc57ab794b11
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-11-06 23:44:07 -0500

    added portfolio header

---
+++ src/components/Headers/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function PortfolioHeader() {
+
+    return (
+        <div class="row py-3 py-md-4 bg-gray">
+            <div class="col">
+                <h3 class="text-white text-center">CODING PORTFOLIO</h3>
+            </div>
+        </div>
+    );
+}
+
+export default PortfolioHeader;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Container } from "../components/Grid";
 import Portfolios from "../components/Portfolios";
+import PortfolioHeader from "../components/Headers";
 
 const Portfolio = () => {
     return (
         <Container fluid>
+            <PortfolioHeader />
             <Portfolios />
         </Container>
     );
