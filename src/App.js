fe83a9585eb6ec1f38d9f706aba67e408af40991
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-11-06 22:47:06 -0500

    enabled portfolio content

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
-import Contact from "./pages/Contact";
+// import Contact from "./pages/Contact";
 import Nav from "./components/Nav";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 // import { StoreProvider } from "./utils/GlobalState";
 
 function App() {
@@ -12,12 +12,12 @@ function App() {
     <Router>
       <div>
         <Nav />
-        {/* <Switch> */}
+        <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/home" component={Home} />
-        {/* <Route exact path="/portfolio" component={Portfolio} /> */}
+        <Route exact path="/portfolio" component={Portfolio} />
         {/* <Route exact path="/contact" component={Contact} /> */}
-        {/* </Switch> */}
+        </Switch>
         {/* <Footer /> */}
       </div>
     </Router>
