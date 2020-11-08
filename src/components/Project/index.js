b55ae6e952ddaeb30eafe588167aa752b373e469
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-11-08 11:58:42 -0500

    remaining projects display

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,14 +3,14 @@ import React from "react";
 function Project(props) {
 
     return (
-        <div key={props.portfolioData.name.toString()}>
-            <div className="col-lg-6 py-lg-5 pt-4 pb-3">
+        <div className="row py-5 border-bottom border-white" key={props.portfolioData.name.toString()}>
+            <div className="col-lg-6">
                 <div className="card text-center border border-secondary">
                     <img src={props.portfolioData.image} className="card-img-top position-relative"
                         alt={props.portfolioData.name} />
                 </div>
             </div>
-            <div className="col-lg-6 py-lg-5 py-3 px-3">
+            <div className="col-lg-6 px-3">
                 <div className="row px-3">
                     <h4 className="text-white text-center">{props.portfolioData.name}</h4>
                     <p className="text-white">{props.portfolioData.description}</p>
@@ -26,7 +26,7 @@ function Project(props) {
                         <a className="btn bg-blue text-white w-75" href={props.portfolioData.code}
                             target="_blank" rel="noreferrer noopener">CODE</a>
                         <a className="btn bg-blue text-white w-75" href={props.portfolioData.application}
-                            target="_blank" rel="noreferrer noopener">APPLICATION</a>
+                            target="_blank" rel="noreferrer noopener">APPLICATION / DEMO</a>
                     </div>
                 </div>
             </div>
