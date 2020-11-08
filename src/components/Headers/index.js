1c5e4444dd51bfe37ad9f75de903ddbcbf51190c
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-11-08 11:57:01 -0500

    style changes

--- src/components/Headers/index.js
+++ src/components/Headers/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function PortfolioHeader() {
 
     return (
-        <div class="row py-3 py-md-4 bg-gray">
+        <div class="row pt-3 pt-md-4 bg-gray">
             <div class="col">
                 <h3 class="text-white text-center">CODING PORTFOLIO</h3>
             </div>
