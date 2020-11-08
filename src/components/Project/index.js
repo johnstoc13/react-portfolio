662d85d120fec1fbb9e6c4d347f3a0651861eff2
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-11-08 18:17:05 -0500

    last style changes

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,14 +3,14 @@ import React from "react";
 function Project(props) {
 
     return (
-        <div className="row py-5 border-bottom border-white" key={props.portfolioData.name.toString()}>
-            <div className="col-lg-6">
+        <div className="row pt-3 pb-4 border-bottom border-white" key={props.portfolioData.name.toString()}>
+            <div className="col-lg-6 pt-4 pb-3">
                 <div className="card text-center border border-secondary">
                     <img src={props.portfolioData.image} className="card-img-top position-relative"
                         alt={props.portfolioData.name} />
                 </div>
             </div>
-            <div className="col-lg-6 px-3">
+            <div className="col-lg-6 pt-4 pb-3 px-3">
                 <div className="row px-3">
                     <h4 className="text-white text-center">{props.portfolioData.name}</h4>
                     <p className="text-white">{props.portfolioData.description}</p>
@@ -26,10 +26,11 @@ function Project(props) {
                         <a className="btn bg-blue text-white w-75" href={props.portfolioData.code}
                             target="_blank" rel="noreferrer noopener">CODE</a>
                         <a className="btn bg-blue text-white w-75" href={props.portfolioData.application}
-                            target="_blank" rel="noreferrer noopener">APPLICATION / DEMO</a>
+                            target="_blank" rel="noreferrer noopener">APPLICATION</a>
                     </div>
                 </div>
             </div>
+            <hr className="primary" />
         </div>
     );
 }
