6134c140979bfdba92433e475e404ef140f389c7
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-11-09 11:02:40 -0500

    navlink change

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./style.css";
 
 function Nav() {
@@ -12,14 +13,17 @@ function Nav() {
                 <div className="col-md-6 my-md-2">
                     <ul className="navbar-nav flex-row justify-content-md-end justify-content-center">
                         <li className="nav-item">
-                            <a className="nav-link text-white px-2 mx-2" href="/">ABOUT</a>
+                            <NavLink to="/" className={window.location.pathname === "/" || window.location.pathname === "/home" ? "nav-link-active" : "nav-link"}>ABOUT</NavLink>
+                            {/* <a className="nav-link text-white px-2 mx-2" href="/">ABOUT</a> */}
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-white px-3 py-0 mt-2 border-left border-right"
-                                href="/portfolio">PORTFOLIO</a>
+                        <NavLink to="/" className={window.location.pathname === "/portfolio" ? "nav-link-active" : "nav-link"}>PORTFOLIO</NavLink>
+                            {/* <a className="nav-link text-white px-3 py-0 mt-2 border-left border-right"
+                                href="/portfolio">PORTFOLIO</a> */}
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-white px-2 mx-2" href="/contact">CONTACT</a>
+                        <NavLink to="/" className={window.location.pathname === "/contact" ? "nav-link-active" : "nav-link"}>CONTACT</NavLink>
+                            {/* <a className="nav-link text-white px-2 mx-2" href="/contact">CONTACT</a> */}
                         </li>
                     </ul>
                 </div>
