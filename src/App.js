de806db08919eeffdda276c17c78682fed1b02aa
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-11-09 07:20:48 -0500

    formatting fix

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
       <div>
         <Nav />
         <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
         </Switch>
         <Footer />
       </div>
