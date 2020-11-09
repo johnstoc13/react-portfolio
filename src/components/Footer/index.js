fe5b5b6b53fde3b848d4c11ac6d0c26e2528cfd6
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-11-09 12:52:45 -0500

    final style changes and nav all working!

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,10 +8,10 @@ function Footer() {
             <div className="navbar fixed-bottom navbar-dark bg-gray justify-content-center">
                 <span className="navbar-brand footer-font blue align-middle">Copyright &copy;2020 | Chase Johnston</span>
                 <span>
-                    <i className="fab fa-github fa-lg blue pr-3" href="https://github.com/johnstoc13" target="_blank"></i>
-                    <i className="fab fa-linkedin fa-lg blue pr-3" href="https://www.linkedin.com/in/chase-johnston8"
-                        target="_blank"></i>
-                    <i className="fas fa-envelope fa-lg blue" href="mailto:chasej@live.com" target="_blank"></i>
+                    <a className="fab fa-github fa-lg hover-orange blue pr-3" href="https://github.com/johnstoc13" target="_blank"></a>
+                    <a className="fab fa-linkedin fa-lg hover-orange blue pr-3" href="https://www.linkedin.com/in/chase-johnston8"
+                        target="_blank"></a>
+                    <a className="fas fa-envelope fa-lg hover-orange blue" href="mailto:chasej@live.com" target="_blank"></a>
                 </span>
             </div>
         </footer>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,23 +6,23 @@ function Nav() {
 
     return (
         <nav className="container-fluid bg-blue border-bottom border-dark">
-            <div className="row">
+            <div className="row py-1">
                 <div className="col-md-6 mt-sm-2 mt-md-0 d-flex justify-content-center align-items-center">
                     <span className="h1 mb-0 text-white">CHASE JOHNSTON</span>
                 </div>
                 <div className="col-md-6 my-md-2">
                     <ul className="navbar-nav flex-row justify-content-md-end justify-content-center">
-                        <li className="nav-item">
-                            <NavLink to="/home" className={window.location.pathname === "/" || window.location.pathname === "/home" ? "nav-link-active" : "nav-link"}>ABOUT</NavLink>
+                        <li className="nav-item px-2 mt-1 mx-2">
+                            <NavLink to="/home" className={window.location.pathname === "/" || window.location.pathname === "/home" ? "nav-link active hover-orange" : "nav-link hover-orange"}>ABOUT</NavLink>
                             {/* <a className="nav-link text-white px-2 mx-2" href="/">ABOUT</a> */}
                         </li>
-                        <li className="nav-item">
-                            <NavLink to="/portfolio" className={window.location.pathname === "/portfolio" ? "nav-link-active" : "nav-link"}>PORTFOLIO</NavLink>
+                        <li className="nav-item px-3 py-0 mt-1 border-left border-right">
+                            <NavLink to="/portfolio" className={window.location.pathname === "/portfolio" ? "nav-link active hover-orange" : "nav-link hover-orange"}>PORTFOLIO</NavLink>
                             {/* <a className="nav-link text-white px-3 py-0 mt-2 border-left border-right"
                                 href="/portfolio">PORTFOLIO</a> */}
                         </li>
-                        <li className="nav-item">
-                            <NavLink to="/contact" className={window.location.pathname === "/contact" ? "nav-link-active" : "nav-link"}>CONTACT</NavLink>
+                        <li className="nav-item px-2 mt-1 mx-2">
+                            <NavLink to="/contact" className={window.location.pathname === "/contact" ? "nav-link active hover-orange" : "nav-link hover-orange"}>CONTACT</NavLink>
                             {/* <a className="nav-link text-white px-2 mx-2" href="/contact">CONTACT</a> */}
                         </li>
                     </ul>
